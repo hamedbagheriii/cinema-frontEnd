08dd95668d63d673a3a40bd1a50431f1e00cbd1c
hamed bagheri <hamedbagheri1386.19@gmail.com>
2024-12-03 23:44:19 +0330

    PART39-adminSidebar(endStyles)

--- components/layout/dashboard/admin/AdminSidebar.tsx
+++ components/layout/dashboard/admin/AdminSidebar.tsx
@@ -55,33 +55,57 @@ const AdminSidebar: FC<sidebarProps> = ({ isSidebar, setSidebar, isUser }) => {
         <hr className='mb-4' />
 
         {/* sidebar content */}
-        <div dir='ltr' className='flex text-left space-y-6 flex-col w-full '>
-          <LinkCompo
-            title='مدیریت فیلم ها'
-            iconClass='person-add me-2'
-            linkClass='mt-2 pl-3'
-            path={'/auth/login'}
-          />
-          <hr />
-          <LinkCompo
-            title='مدیریت سینما ها'
-            iconClass='person-add me-2'
-            linkClass='mt-2 pl-3'
-            path={'/auth/login'}
-          />
-          <hr />
-          <LinkCompo
-            title='مدیریت بلیط ها'
-            iconClass='person-add me-2'
-            linkClass='mt-2 pl-3'
-            path={'/auth/login'}
-          />
-          <hr />
+        <div dir='ltr' className='flex text-left space-y-6 mb-28 flex-col w-full '>
           <Accordion type='single' collapsible>
             <AccordionItem value='item-1' className='text-white Accordion '>
               <AccordionTrigger
                 className={`text-[16px] text-white px-3
-                mb-3 decoration-transparent font-normal hover:bg-black/50 
+                mb-3 decoration-transparent font-normal bg-red-900 hover:bg-black/70 
+                transition-all duration-150 rounded-md 
+                ${handleCheckLink('/dashboard/admin/users', router)}`}
+              >
+                <div className='flex gap-2 text-center '>
+                  <i className='bi bi-columns-gap mt-0.5'></i>
+                  مدیریت سینما
+                </div>
+              </AccordionTrigger>
+
+              <AccordionContent className='flex flex-col space-y-3 '>
+                <hr className='w-full' />
+                <div className='flex flex-col space-y-3 pl-2'>
+                  <LinkCompo
+                    title='مدیریت فیلم ها'
+                    iconClass='person-add me-2'
+                    linkClass='pl-8'
+                    border={true}
+                    path={'/auth/login'}
+                  />
+                  <hr />
+                  <LinkCompo
+                    title='مدیریت سینما ها'
+                    iconClass='person-add me-2'
+                    linkClass='pl-8'
+                    border={true}
+                    path={'/auth/login'}
+                  />
+                  <hr />
+                  <LinkCompo
+                    title='مدیریت بلیط ها'
+                    iconClass='person-add me-2'
+                    linkClass='pl-8'
+                    border={true}
+                    path={'/auth/login'}
+                  />
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion type='single' collapsible>
+            <AccordionItem value='item-1' className='text-white Accordion '>
+              <AccordionTrigger
+                className={`text-[16px] text-white px-3
+                mb-3 decoration-transparent font-normal bg-red-900 hover:bg-black/70 
                 transition-all duration-150 rounded-md 
                 ${handleCheckLink('/dashboard/admin/users', router)}`}
               >
@@ -93,54 +117,42 @@ const AdminSidebar: FC<sidebarProps> = ({ isSidebar, setSidebar, isUser }) => {
 
               <AccordionContent className='flex flex-col space-y-3 '>
                 <hr className='w-full' />
-                <LinkCompo
-                  title='کاربران'
-                  linkClass={`pl-8 `}
-                  iconClass='person-circle me-2'
-                  path={'/dashboard/admin/users'}
-                />
+                <div className='flex flex-col space-y-3 pl-2'>
+                  <LinkCompo
+                    title='مشاهده کاربران'
+                    linkClass={`pl-8 `}
+                    iconClass='person-circle me-2'
+                    border={true}
+                    path={'/dashboard/admin/users'}
+                  />
 
-                <hr className='w-11/12 ms-auto' />
-                <LinkCompo
-                  title='کیف پول ها'
-                  iconClass='ticket-perforated me-2 '
-                  linkClass={`pl-8`}
-                  path={'/dashboard/admin/users/wallet'}
-                />
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+                  <hr />
+                  <LinkCompo
+                    title='کیف پول ها'
+                    iconClass='ticket-perforated me-2 '
+                    linkClass={`pl-8`}
+                    border={true}
+                    path={'/dashboard/admin/users/wallet'}
+                  />
 
-          <Accordion type='single' collapsible>
-            <AccordionItem value='item-1' className='text-white Accordion '>
-              <AccordionTrigger
-                className={`text-[16px] text-white px-3
-                mb-3 decoration-transparent font-normal hover:bg-black/50 
-                transition-all duration-150 rounded-md 
-                ${handleCheckLink('/dashboard/admin/users', router)}`}
-              >
-                <div className='flex gap-2 text-center '>
-                  <i className='bi bi-columns-gap mt-0.5'></i>
-                  مدیریت نقش ها
+                  <hr />
+                  <LinkCompo
+                    title='نقش ها'
+                    iconClass='ticket-perforated me-2 '
+                    linkClass={`pl-8`}
+                    border={true}
+                    path={'/dashboard/admin/users/wallet'}
+                  />
+
+                  <hr />
+                  <LinkCompo
+                    title='مجوز ها'
+                    iconClass='ticket-perforated me-2 '
+                    linkClass={`pl-8`}
+                    border={true}
+                    path={'/dashboard/admin/users/wallet'}
+                  />
                 </div>
-              </AccordionTrigger>
-
-              <AccordionContent className='flex flex-col space-y-3 '>
-                <hr className='w-full' />
-                <LinkCompo
-                  title='کاربران'
-                  linkClass={`pl-8 `}
-                  iconClass='person-circle me-2'
-                  path={'/dashboard/admin/users'}
-                />
-
-                <hr className='w-11/12 ms-auto' />
-                <LinkCompo
-                  title='کیف پول ها'
-                  iconClass='ticket-perforated me-2 '
-                  linkClass={`pl-8`}
-                  path={'/dashboard/admin/users/wallet'}
-                />
               </AccordionContent>
             </AccordionItem>
           </Accordion>
@@ -148,7 +160,7 @@ const AdminSidebar: FC<sidebarProps> = ({ isSidebar, setSidebar, isUser }) => {
 
         {/* sidebar footer */}
         {isUser && (
-          <div className='mt-auto w-full  flex-col mb-2 flex'>
+          <div className='mt-auto w-full flex-col mb-2 flex'>
             <hr className='w-full' />
             <ConfirmAlert
               title='آیا میخواهد از حساب کاربری خارج شوید ؟'
