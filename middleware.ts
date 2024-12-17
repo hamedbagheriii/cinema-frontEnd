0963fa16a2eacf5af786253527ef9ff6e315f2de
hamed bagheri <hamedbagheri1386.19@gmail.com>
2024-12-17 16:10:14 +0330

    PART56-test9(fix bug in login)

--- middleware.ts
+++ middleware.ts
@@ -128,14 +128,14 @@ export const middleware = async (req: NextRequest) => {
     ) {
       return NextResponse.redirect(new URL('/dashboard/user/profile', req.url));
     }
+  } else if (req.nextUrl.pathname.startsWith('/event') && !isLogin) {
+    return NextResponse.redirect(new URL('/auth/login', req.url));
   } else if (
     isLogin &&
     req.nextUrl.pathname.startsWith('/auth') &&
     !req.nextUrl.pathname.startsWith('/auth/logout')
   ) {
     return NextResponse.redirect(new URL('/', req.url));
-  } else if (req.nextUrl.pathname.startsWith('/event') && !isLogin) {
-    return NextResponse.redirect(new URL('/auth/login', req.url));
   }
 };
 
